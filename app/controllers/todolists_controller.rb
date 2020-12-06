9824d34a26154bb1bb3b78b118a8fd7fd29bf2e5
EC2 Default User <ec2-user@ip-172-31-41-14.ec2.internal>
2020-12-06 05:07:41 +0000

    Finish　5章編集機能を作ろう（編集画面の作成）

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,25 +1,39 @@
 class TodolistsController < ApplicationController
+
+  #空のモデルを作成 投稿フォーム(form_with)と関連させている
   def new
     @list = List.new
   end
 
+  #データを登録するための処理
   def create
+    #データを新規登録するためのインスタンスを生成
+    #list_paramsが呼び出される　※下に定義されている
     list = List.new(list_params)
     list.save
+    #新規投稿後のリダイレクト先
     redirect_to todolist_path(list.id)
   end
 
+  #投稿一覧　allメソッドで全てのデータを取り出す　インスタンス変数は複数形
   def index
     @lists = List.all
   end
 
+  #URL内のidをparams[:id]で受け取る
   def show
     @list = List.find(params[:id])
   end
 
+  #データを編集するために、.findメソッドで保存されたデータを取り出す必要がある
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  #privateより下はコントローラ内の中でしか呼び出せない
   private
+  #privateよりも下に記載されたlist_paramsはアクションとして認識されない
   def list_params
     params.require(:list).permit(:title,:body)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  #投稿フォーム
   get 'todolists/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
+  #データの送信先
   post 'todolists' => 'todolists#create'
+  #一覧画面
   get 'todolists' => 'todolists#index'
+  #詳細画面
   get 'todolists/:id' => 'todolists#show', as: 'todolist'
+  #編集画面
+  get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
 end
